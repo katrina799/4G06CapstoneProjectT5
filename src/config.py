96b1fbe53d05b71cbfced888a8edd3f228411a77
wangq131 <wangq131@mcmaster.ca>
2023-10-25 04:37:00 +0000

    Set up a simple Flask app that can interact with S3

---
+++ src/config.py
@@ -0,0 +1,2 @@
+# A configuration file where all constants are defined
+BUCKET_NAME = "course-buddy"
---
+++ src/poc_data/generate_mock_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+filename = "mock_data_poc.csv"
+mock_data = {
+    "user_id": [1, 2, 3],
+    "username": ["Jane", "Katrina", "Joyce"],
+    "courses": [
+        ["SE4G06", "SE4X03", "ANTHROP1AA4"],
+        ["SE4G06", "STAT3Y03"],
+        ["SE4G06", "SE4X03", "MUSIC2MT3", "ENG1A03"],
+    ],
+}
+df = pd.DataFrame(data=mock_data)
+df.to_csv(f"{os.getcwd()}/{filename}")
