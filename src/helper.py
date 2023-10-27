75b771dffa8300903cf83f2df3dd3b312f22ccb4
wangq131 <wangq131@mcmaster.ca>
2023-10-27 02:22:07 +0000

    Add functions to add and remove courses

--- src/helper.py
+++ src/helper.py
@@ -1,5 +1,6 @@
 # Helper functions that will be commonly used
 import pandas as pd
+import os
 
 
 # Get a specific csv file from s3 and return it as a dataframe
@@ -7,3 +8,14 @@ def get_df_from_csv_in_s3(s3, bucket_name, s3_csv_file_path):
     s3_obj = s3.get_object(Bucket=bucket_name, Key=s3_csv_file_path)
     df = pd.read_csv(s3_obj["Body"])
     return df
+
+
+def upload_df_to_s3(df, s3, bucket_name, s3_csv_file_path):
+    new_csv_file_path = "poc_data/tmp.csv"
+    df.to_csv(new_csv_file_path)
+    s3.upload_file(
+        new_csv_file_path,
+        bucket_name,
+        s3_csv_file_path,
+    )
+    os.remove(new_csv_file_path)
