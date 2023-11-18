933f13cd0b320e87c5c3c387ce3a6ce10292feb8
wangq131 <wangq131@mcmaster.ca>
2023-11-18 00:16:34 +0000

    Add a training pipeline script to automate model training

--- src/config.py
+++ src/config.py
@@ -2,3 +2,7 @@
 BUCKET_NAME = "course-buddy"
 MOCK_DATA_POC_NAME = "mock_data_poc.csv"
 MOCK_DATA_POC_TASKS = "mock_data_tasks.csv"
+PRIORITY_MODEL_FILE_NAME = "trained_priority_model.joblib"
+PRIORITY_MODEL_FILE_PATH = (
+    f"src/task-priority-training-pipeline/{PRIORITY_MODEL_FILE_NAME}"
+)
