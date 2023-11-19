a297ed5ae6951b199a152f8a4e9054fc8ddd4ba9
qinj15 <qinj15@mcmaster.ca>
2023-11-19 01:17:22 +0000

    features are added

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import botocore
 import boto3
 from flask import Flask, render_template, request, Response, redirect, url_for
 import ast
+import pandas as pd
 
 from helper import get_df_from_csv_in_s3, upload_df_to_s3
 
@@ -134,7 +135,7 @@ def plan_page():
     )
 
 
-# Router to user profile page
+# Router to user profile pageile
 @app.route("/profile_page", methods=["GET", "POST"])
 def profile_page():
     # render the profile page, showing username on pege
@@ -145,19 +146,48 @@ def profile_page():
 
 @app.route("/course_detail_page/<course_id>")
 def course_detail(course_id):
-    course = course_id
-    if course:
-        return render_template("course_detail_page.html", course=course)
+    # 假设有一个函数check_syllabus_exists()来检测S3中是否有大纲文件
+    message = request.args.get("message", "")
+    syllabus_exists, syllabus_filename = check_syllabus_exists(course_id)
+
+    if syllabus_exists:
+        # 直接调用get_emails路由来提取邮件
+        email_list = extract_emails_from_pdf(syllabus_filename)
     else:
-        return "Course not found", 404
+        email_list = []
+
+    return render_template(
+        "course_detail_page.html",
+        course_id=course_id,
+        course=course_id,
+        username=username,
+        email_list=email_list,
+        message=message,
+    )
+
+
+def check_syllabus_exists(course_id):
+    try:
+        # 假设大纲文件的命名规则是：course_id + "-syllabus.pdf"
+        syllabus_filename = course_id + "-syllabus.pdf"
+
+        # 检查文件是否存在于S3上
+        s3.head_object(Bucket=bucket_name, Key=syllabus_filename)
+        return True, syllabus_filename
+    except botocore.exceptions.ClientError as e:
+        # 如果文件不存在，head_object会抛出ClientError异常
+        if e.response["Error"]["Code"] == "404":
+            return False, None
+        else:
+            # 如果出现其他错误，重新抛出异常
+            raise e
 
 
 # input:pdf file
 # output: a list of string emails
 # extract all the email in the input pdf
 @app.route("/get_emails", methods=["GET"])
-def extract_emails_from_pdf():
-    filename = request.args.get("filename")
+def extract_emails_from_pdf(filename):
     if request.method == "GET":
         response = s3.get_object(Bucket=bucket_name, Key=filename)
         pdf_file = response["Body"].read()
@@ -173,32 +203,79 @@ def extract_emails_from_pdf():
         email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
         emails = re.findall(email_pattern, text)
 
-        return render_template("index.html", emails=emails)
+        return emails
 
 
-@app.route("/")
-def index():
-    return render_template("upload.html")
-
-
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return "No file selected"
+@app.route("/upload/<course_id>", methods=["POST"])
+def upload_file(course_id):
+    print("course id", course_id)
+    if (
+        "file" not in request.files
+        or not request.files["file"]
+        or request.files["file"].filename == ""
+    ):
+        print("checked")
+        return redirect(
+            url_for(
+                "course_detail",
+                course_id=course_id,
+                message="No file selected",
+                username=username,
+            )
+        )
 
     file = request.files["file"]
-
-    if file.filename == "":
-        return "No file selected"
+    new_filename = f"{course_id}-syllabus.pdf"
+    file.filename = new_filename  # 更新文件名
+    print("file:", file.filename)
 
     try:
         # 使用 upload_fileobj 方法直接上传
+        print("uploading")
         s3.upload_fileobj(
             file, bucket_name, file.filename, ExtraArgs={"ACL": "private"}
         )
-        return "File uploaded successfully!"
+        # 确保update_csv函数已经定义
+        update_csv(course_id, file.filename)
+        print("returning")
+        return redirect(
+            url_for(
+                "course_detail",
+                course_id=course_id,
+                message="File uploaded successfully!",
+                username=username,
+            )
+        )
     except botocore.exceptions.NoCredentialsError:
-        return "AWS authentication failed. Please check your AWS keys."
+        print("error")
+        return redirect(
+            url_for(
+                "course_detail",
+                course_id=course_id,
+                message="AWS authentication failed. Please check your AWS keys.",
+                username=username,
+            )
+        )
+
+
+def update_csv(course_id, syllabus_filename):
+    csv_file_path = "./poc-data/moc_course_info.csv"  # 更改为实际的文件路径
+
+    # 读取CSV文件，删除空行
+    df = pd.read_csv(csv_file_path).dropna(how="all")
+
+    # 检查course_id是否在非空的单元格中
+    if course_id in df["course"].dropna().values:
+        df.loc[df["course"] == course_id, "course_syllabus"] = syllabus_filename
+    else:
+        # 创建一个新的DataFrame来添加新行
+        new_row = pd.DataFrame(
+            {"course": [course_id], "course_syllabus": [syllabus_filename]}
+        )
+        df = pd.concat([df, new_row], ignore_index=True)
+
+    # 写回CSV文件
+    df.to_csv(csv_file_path, index=False)
 
 
 if __name__ == "__main__":
