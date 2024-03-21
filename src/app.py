6b5c5c300302bd45751feb8713c963ba62ab4e78
Chenwei <song6479790026@gamil.com>
2024-03-20 21:52:16 -0400

    region name

--- src/app.py
+++ src/app.py
@@ -94,7 +94,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-    region_name="us-east-2",
+    region_name=app.config["REGION_NAME"],
 )
 
 tomato_data_key = "weekly_tomato_data.csv"
